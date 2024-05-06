cc531ac0cf061eb698197f8de83cbe36cdf5b374
Andreas <andreas3739@gmail.com>
2024-05-06 12:05:09 +0800

    added setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setup(
+    name="me_gau",
+    version="v0.0.1",
+    description="""A tool to perdict face action unit (FAU) and emotion from video.""",
+    long_description_content_type="text/markdown",
+    author="Andreas Susanto",
+    packages=find_packages(include=["vcab"]),
+    package_data={'': ['*.ckpt']},
+    install_requires=required,
+    license="MIT",
+    url="https://github.com/Andreas-UI/ME-GraphAU-Video.git",
+    python_requires=">=3.10",
+)
